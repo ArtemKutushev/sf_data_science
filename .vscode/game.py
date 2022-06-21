5766a1a98f87990a57cf21605e4ba77d0cfc2be3
Artem Kutushev <kutushev.ao@inbox.ru>
2022-06-21 16:21:56 +0300

    Initial commit

---
+++ .vscode/game.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+number = np.random.randint(1, 101) # загадываем число
+
+count = 0
+
+while True:
+    count += 1
+    predict_number = int(input("Введите число от 1 до 100   "))
+    if predict_number > number:
+        print("Число должно быть меньше!")
+    elif predict_number < number:
+        print("Число должно быть больше!")
+    else:
+        print(f"Вы угадали число! Угаданное число: {number} за {count} попыток.")
+        break #игра закончена
+ 
