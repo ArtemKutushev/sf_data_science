0bec7a365c1b98a3f714a7298c9e4dcecf36de05
Artem Kutushev <kutushev.ao@inbox.ru>
2022-06-22 14:56:45 +0300

    readme

--- project_0/game_v2.py
+++ project_0/game_v2.py
@@ -1,22 +1,23 @@
 import numpy as np
-def random_predict(number:int=1)->int:
+
+def random_predict(number:int=1) -> int:
     """Рандомно угадываем число
 
     Args:
-        number (int, optional): загаданное число. Defaults to 1.
+        number (int, optional): Загаданное число. Defaults to 1.
 
     Returns:
-        int: число попыток
+        int: Число попыток
     """
+
     count = 0
+
     while True:
         count += 1
-        predict_number = np.random.randint(1, 101) #предполагаемое число
-        if predict_number == number:
+        predict_number = np.random.randint(1, 101) # предполагаемое число
+        if number == predict_number:
             break
-    return(count) 
-
-print(f'Количество попыток: {random_predict()}')
+    return(count)
 
 def score_game(random_predict) -> int:
     """За какое количество попыток в среднем из 1000 подходов угадывает наш алгоритм
@@ -40,10 +41,5 @@ def score_game(random_predict) -> int:
     print(f'Ваш алгоритм угадывает число в среднем за: {score} попыток')
     return(score)
 
-# RUN
 if __name__ == '__main__':
     score_game(random_predict)
-
-
-
-    
